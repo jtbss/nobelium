56b63def90af99891eeaa3424e9e11ea9fc2c6b9
GamTin <724810114@qq.com>
2022-07-18 18:48:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,6 +11,7 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
+  THEME: 'hexo',
   postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
